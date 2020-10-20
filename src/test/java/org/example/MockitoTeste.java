06532ba0e469716fa3e23b4a1e8b493a58af7c21
alexsandraholanda <alexsandraale8795@gmail.com>
2020-10-20 00:18:11 -0300

    Biblioteca Mockito

---
+++ src/test/java/org/example/MockitoTeste.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+import org.mockito.Mockito;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MockitoTeste {
+
+    @Test
+    public void primeiroTesteMockito(){
+        List<String> lista = Mockito.mock(ArrayList.class);
+
+        Mockito.when( lista.size()).thenReturn(10);
+        int size = lista.size();
+        Assertions.assertThat(size).isEqualTo(10);
+    }
+}
