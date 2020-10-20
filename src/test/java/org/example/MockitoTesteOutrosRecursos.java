59438f67d9e37f0e84c86e2b9f34d184462b5a27
alexsandraholanda <alexsandraale8795@gmail.com>
2020-10-20 00:42:20 -0300

    Alguns recursos Mockito

---
+++ src/test/java/org/example/MockitoTesteOutrosRecursos.java
@@ -0,0 +1,43 @@
+package org.example;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InOrder;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@RunWith(MockitoJUnitRunner.class)
+public class MockitoTesteOutrosRecursos {
+
+    @Mock
+    List<String> lista;
+
+    @Test
+    public void primeiroTesteMockito(){
+       Mockito.mock(ArrayList.class);
+
+        Mockito.when( lista.size()).thenReturn(10);
+        int size = lista.size();
+        Assertions.assertThat(size).isEqualTo(10);
+
+        //Verifica se chamou o metodo;
+            //Mockito.verify(lista).size();
+
+        //Verifica quantas vezes o metodo foi chamado. (size())
+            //Mockito.verify(lista, Mockito.times(1)).size();
+
+        //Quando espera que um determinado procedimento não tenha sido executado no seu teste.
+            //Mockito.verify(lista, Mockito.never()).size();
+
+        //Verifica que os testes foram execultado em uma determinada ordem.
+            //InOrder inOrder = Mockito.inOrder(lista);
+
+
+    }
+}
